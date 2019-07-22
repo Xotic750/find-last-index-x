c41e17e8163de9240fe54c6d95eaf10e8eb9534b
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:31:20 +0200

    :bookmark: v3.0.4
    Dependency updates

--- dist/find-last-index-x.js
+++ dist/find-last-index-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-21T23:30:00.441Z",
+  "date": "2019-07-22T13:31:13.353Z",
   "describe": "",
   "description": "Search an array from the end and return the matched index.",
   "file": "find-last-index-x.js",
   "hash": "f29382b79722ed4edb0a",
   "license": "MIT",
-  "version": "3.0.3"
+  "version": "3.0.4"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
