b4d063d439456a6c999c2df5a11983fe63d53aea
Graham Fairweather <xotic750@gmail.com>
2019-08-27 23:22:13 +0200

    Maintenance

--- dist/find-last-index-x.js
+++ dist/find-last-index-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-20T20:29:46.409Z",
+  "date": "2019-08-27T21:22:06.326Z",
   "describe": "",
   "description": "Search an array from the end and return the matched index.",
   "file": "find-last-index-x.js",
